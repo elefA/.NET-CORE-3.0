fe68139ad533009959ecafd951cf9cef03c96c7f
Aris Eleftheriadis <59826921+elefA@users.noreply.github.com>
2020-05-04 05:37:00 +0300

    Initial commit

---
+++ BookListRazor/BookListRazor/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key] // Using this import Key from system.componentModel.DataAnnotation is Auto Increment
+        public int Id { get; set; }
+
+        [Required] //Required Field
+        public string Name{ get; set; }
+
+        public string Author { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# .NET CORE 3.0
+ A project done with MVC and Razor pages separately
